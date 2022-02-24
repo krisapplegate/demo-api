4e6d604dfd5547b1ed6f1006748adbfde0f0041a
Kris Applegate <kapplegate@vmware.com>
2022-02-24 08:54:35 -0600

    commit

--- src/main/java/com/example/restserviceinitial/RestServiceInitialApplication.java
+++ src/main/java/com/example/restserviceinitial/RestServiceInitialApplication.java
@@ -1,5 +1,7 @@
 package com.example.restserviceinitial;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestServiceInitialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestServiceInitialApplication.class, args);
+		SpringApplication app = new SpringApplication(RestServiceInitialApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "80"));
+        app.run(args);
 	}
 
 }
